f8d7aa217781d6fbf9173f66c92a7ae7bee4a808
Lior Perlin <lior.perlin@post.runi.ac.il>
2025-11-20 21:08:42 +0200

    Added CalcPi.Java

--- CalcPi.java
+++ CalcPi.java
@@ -1,7 +1,27 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-		String str = "Check";
+	    int amount = Integer.parseInt(args[0]);
+		if (amount <= 0) {
+			System.out.println("Give me a non-negative integer");
+		}
+		else {
+			double sum = 1.0;
+			int denominator = 3;
+			double temp;
+			for(int i = 0; i < amount; i++) {
+				if(i % 2 == 0) {
+					temp = (-1.0) / denominator;
+				}
+				else {
+					temp = 1.0 / denominator;
+				}
+				sum += temp;
+				denominator += 2;
+			}
+			System.out.println("pi according to Java:	" + Math.PI);
+			sum *= 4;
+			System.out.println("pi, approximated:	" + sum);
+		}
 	}
 }
